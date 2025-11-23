3b43723cfbd4fadd5acd20b1b1bbe485132193b9
alexeyshmelev <justadocs@gmail.com>
2025-11-23 03:27:59 +0300

    update

--- downstream_tasks/real_data_simulation_class_balance_interpolation/run_finetune.sh
+++ downstream_tasks/real_data_simulation_class_balance_interpolation/run_finetune.sh
@@ -2,4 +2,4 @@
 cd ../..
 JSON_PATH=./downstream_tasks/real_data_simulation_class_balance_interpolation
 python utils/pipeline.py --data_folder /disk/10tb/home/shmelev/genlink_real_data_simulation_class_balance_interpolation --hardware "0|1" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params.json \
---models_per_gpu 1
+--models_per_gpu 2
