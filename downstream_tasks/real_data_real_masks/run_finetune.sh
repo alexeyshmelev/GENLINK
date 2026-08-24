f9e90388c9e5472e5202a5bfaa687d5a84e7da71
alexeyshmelev <justadocs@gmail.com>
2026-08-24 18:56:23 +0300

    new ES pictures

--- downstream_tasks/real_data_real_masks/run_finetune.sh
+++ downstream_tasks/real_data_real_masks/run_finetune.sh
@@ -3,5 +3,5 @@ PROCESS_NAME="torchrun --master_addr 127.0.0.1 --nproc_per_node 5 segmentation/t
 
 cd ../..
 JSON_PATH=./downstream_tasks/real_data_real_masks
-python utils/pipeline.py --data_folder /home/jovyan/shares/SR003.nfs2/GENATATOR_PIPELINE/final_datasets/2nd_degree/unlabeled_pca_needed --hardware "0|1|2|3|4|5|6" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params.json \
---models_per_gpu 2
+python utils/pipeline.py --data_folder /home/jovyan/shares/SR003.nfs2/GENATATOR_PIPELINE/final_datasets/2nd_degree/unlabeled_scaled --hardware "0|1|2|3|4|5|6" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params.json \
+--models_per_gpu 3
