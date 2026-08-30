40e204d9fe987e8df46762082334f374c7558592
alexeyshmelev <justadocs@gmail.com>
2026-08-31 00:47:38 +0300

    es v2 update

--- downstream_tasks/real_data_no_masks/run_finetune_no_pca.sh
+++ downstream_tasks/real_data_no_masks/run_finetune_no_pca.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-PROCESS_NAME="torchrun --master_addr 127.0.0.1 --nproc_per_node 5 segmentation/train.py --config segmentation/configs/moderngena_base_gpt.json"
+PROCESS_NAME="torchrun --master_addr 127.0.0.1 --nproc_per_node 7 segmentation/train.py --config segmentation/configs/moderngena_base_gpt.json"
 cd ../..
 JSON_PATH=./downstream_tasks/real_data_no_masks
-python utils/pipeline.py --data_folder /home/jovyan/shares/SR003.nfs2/GENATATOR_PIPELINE/final_datasets/2nd_degree/labaled_pca_needed --hardware "0|1|2|3|4|5|6" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params_no_pca.json \
+python utils/pipeline.py --data_folder /home/jovyan/shares/SR003.nfs2/GENATATOR_PIPELINE/final_datasets_2/2nd_degree/labeled --hardware "0|1|2|3|4|5|6" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params_no_pca.json \
 --models_per_gpu 2
