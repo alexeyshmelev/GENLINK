75217b0cfaae2f838fd0d61f803dadb8ef7ef82b
alexeyshmelev <justadocs@gmail.com>
2026-09-18 02:43:30 +0300

    ES update

--- downstream_tasks/real_data_no_masks/run_finetune_no_pca.sh
+++ downstream_tasks/real_data_no_masks/run_finetune_no_pca.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-set -e
-# change dir to the repository root
-cd /home/jovyan/GENLINK
-
-source /home/jovyan/miniconda3/bin/activate
-conda activate /home/jovyan/miniconda3/envs/genlink
+cd ../..
 
 JSON_PATH=./downstream_tasks/real_data_no_masks
-python utils/pipeline.py --data_folder /home/jovyan/final_datasets_2/all_degree/labeled --hardware "0|1|2|3|4|5|6|7" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params_no_pca.json \
---models_per_gpu 2
+python utils/pipeline.py --data_folder /home/jovyan/shares/SR003.nfs2/GENATATOR_PIPELINE/final_datasets_2/2nd_degree/labeled_only --hardware "0|1|2|3|4|5|6" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params_no_pca.json \
+--models_per_gpu 3
--- downstream_tasks/real_data_real_masks/run_finetune.sh
+++ downstream_tasks/real_data_real_masks/run_finetune.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-set -e
-# change dir to the repository root
-cd /home/jovyan/GENLINK
-
-source /home/jovyan/miniconda3/bin/activate
-conda activate /home/jovyan/miniconda3/envs/rapids-25.06
+cd ../..
 
 JSON_PATH=./downstream_tasks/real_data_real_masks
-python utils/pipeline.py --data_folder /home/jovyan/final_datasets_2/2nd_degree/unlabeled_partial --hardware "0|1|2|3|4|5|6|7" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params.json --models_per_gpu 1
+python utils/pipeline.py --data_folder /home/jovyan/shares/SR003.nfs2/GENATATOR_PIPELINE/final_datasets_2/2nd_degree/all_unlabeled_with_pca/ --hardware "4|5" --model_list ${JSON_PATH}/model_list.json --running_params ${JSON_PATH}/running_params.json --models_per_gpu 1
